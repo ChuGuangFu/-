e419982f977198a50016f12f29a2f214ef5b0244
处光夫 <chuguangfu@chuguangfudeiMac.local>
2018-12-11 15:34:27 +0800

    Initial Commit

---
+++ 下拉手势退出/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  下拉手势退出
+//
+//  Created by 处光夫 on 2018/12/11.
+//  Copyright © 2018 处光夫. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 下拉手势退出/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  下拉手势退出
+//
+//  Created by 处光夫 on 2018/12/11.
+//  Copyright © 2018 处光夫. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
